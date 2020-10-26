330cb8920251f46bbd86f0de8b09779de2d058ff
Kyle Thomas <kyle.thomas.dev@gmail.com>
2020-10-26 20:08:05 +0000

    Added new scene

--- main.lua
+++ main.lua
@@ -5,3 +5,12 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+local composer = require( "composer" )
+
+-- Setup theme
+local background = display.newImageRect( "kinetic_logo.png", 270, 66 )
+background.x = display.contentCenterX
+background.y = 60
+
+-- Merchant starts a new transaction
+composer.gotoScene( "new" )
